3df89cc3851495b72ad8db7bb86bc613d96638a0
Abhishek <abhisingh8734@gmail.com>
2025-07-10 18:26:48 +0530

    Learned about JavaScript

---
+++ JavaScript/first.js
@@ -0,0 +1,9 @@
+console.log("Welcome, Abhishek Singh!");
+fullName = "Abhishek singh";
+age = 24;
+price = 30.44;
+x = null; // null has special value basically it is empty 
+y = undefined;
+console.log(fullName);
+console.log(y);
+console.log(x);
